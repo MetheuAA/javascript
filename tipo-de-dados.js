2d9ab0f0a4fff57dfb7134987cbebbc718db3ef7
MetheuAA <matheo200817@gmail.com>
2024-03-27 08:51:59 -0400

    add tipo de dados

---
+++ tipo-de-dados.js
@@ -0,0 +1,27 @@
+// // // var nome = "Felipe";
+// // // console.log(typeof(nome));
+
+// // // caracter = 'F';
+// // // console.log(typeof(caracter));
+
+// // // var idade = 25
+// // // console.log(typeof(idade));
+
+// // // var salario = 1.500;
+// // // console.log(typeof(salario));
+
+// // var ligada = false;
+// // console.log (typeof(ligada));
+
+// // var desligada = false;
+// // console.log (typeof(desligada));
+
+// var a = 10;
+// console.log(typeof(a));
+// var b = "10";
+// console.log(typeof(b));
+
+// console.log(a == b);
+// console.log(a === b);
+
+console.log(true > false);
